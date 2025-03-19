ac5007e01fa9408bcf07cbae33e3ca3be53daed5
Daanyong <101034775+Daanyong@users.noreply.github.com>
2025-03-19 11:15:22 +0900

    L04 homework 업로드

---
+++ L04/L04_01.py
@@ -0,0 +1,25 @@
+# 이진화 및 히스토그램 구하기
+# 주어진 이미지를 불러와서 다음을 수행
+# 이미지를 그레이스케일로 변환
+# 특정 임계값을 설정하여 이진화
+# 이진화된 이미지의 히스토그램을 계산하고 시각화
+
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+# 요구사항 1: cv.imread()를 사용하여 이미지 로드
+img = cv.imread('soccer.jpg')
+
+if img is None:
+    sys.exit('파일이 존재하지 않습니다.')
+
+# 요구사항 2: cv.cvtColor()를 사용해 이미지를 그레이스케일로 변환
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# 요구사항 3: cv.threshold()를 사용해 이진화, 임계값: 127
+retval, binary = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+
+# 요구사항 4: cv.calcHist()를 사용해 히스토그램을 계산하고, matplotlib으로 시각화
+hist = cv.calcHist([gray], [0], None, [256], [0, 256])
+plt.plot(hist, color='r', linewidth=1)
+plt.show()
